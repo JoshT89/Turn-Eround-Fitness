1a38e0fdd5d582b7c2c817fb1d913dddc00b1005
JoshT89 <josh.h.t@live.co.uk>
2023-11-22 18:19:51 +0000

    first commit

---
+++ src/components/BodyPart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import Icon from '../assets/icons/fitness.png';
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  <Stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className="bodyPart-card"
+    sx={bodyPart === item ? {  borderTop: '4px solid #0BBD1B', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    onClick={() => {
+      setBodyPart(item);
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+    }}
+  >
+      <img src={Icon} alt="dumbbell" style={{ width: '150px', height: '150px' }} />
+    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#00646D" textTransform="capitalize"> {item}</Typography>
+  </Stack>
+);
+
+export default BodyPart;
